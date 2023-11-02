d3afd5dd07208c9fe262da06fe34903dac8a3f75
Anton-Nevezhin <anton-nevezhin@yandex.ru>
2023-11-02 11:09:32 +0300

    Add second page

--- src/lib/scripts.js
+++
@@ -1,1 +0,0 @@
-console.log('Из файла scripts.js')
--- src/lib/style.js
+++ src/lib/style.js
@@ -2,6 +2,5 @@
 
 // Place other JavaScript modules in the 'lib' folder
 import 'assets/styles/index.less'
+import 'assets/styles/single.less'
 console.log('Style applied')
-
-document.getElementById('root').innerHTML = 'Из файла style.js'
