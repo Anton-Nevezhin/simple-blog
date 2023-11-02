f58ad3d7868c677a001fa52b137f6eb0230e24eb
anton-nevezhin <anton-nevezhin@yandex.ru>
2023-11-02 18:25:45 +0300

    New image

--- src/scripts/single.js
+++ src/scripts/single.js
@@ -28,7 +28,8 @@ for (let i = 0; i < records.length; i++) {
   // singleImage.src = '/simple-blog/dist/static/img/02.jpg'
   // let add = '01.png'
   // console.log('/simple-blog/dist/static/img/' + add)
-  singleImage.src = 'static/img/01.83d35ee.png'
+  // singleImage.src = 'static/img/01.83d35ee.png'
+  singleImage.src = 'static/img/02.jpg'
   // singleImage.src = './post-1.png'
   blogcardText.appendChild(card)
   blogcardText.appendChild(singleText)
