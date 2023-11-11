757255eb5a02d7ccc333761fccf6e104c05edb3a
anton-nevezhin <anton-nevezhin@yandex.ru>
2023-11-11 23:00:37 +0300

    Add edit

--- src/scripts/single.js
+++ src/scripts/single.js
@@ -1,16 +1,21 @@
 import '../lib/style'
 
-import { rec } from './config'
-console.log('Из config: ', rec)
+// import { rec } from './config'
+// console.log('Из config: ', rec)
 
-import { hello } from '../lib/style.js'
-console.log('Из style: ', hello) // Выведет "Hello, World!"
+// import { hello } from '../lib/style.js'
+// console.log('Из style: ', hello) // Выведет "Hello, World!"
 
 // import { records } from './index.js'
 
-export let recSingle = 'Из single'
+// export let ls = 'Из сингл'
 
-export let textSingle = 'Пример текста из single'
+// export let recSingle = 'Из single'
+
+// export let textSingle = 'Пример текста из single'
+
+let sin = localStorage.getItem('mas')
+console.log('mas in single: ', sin)
 
 const records = [
   {title: 'Запись 1', text: 'Путешествия по морю – это непрерывная и захватывающая одиссея, которая влечет за собой приключения, открывает новые горизонты и позволяет ощутить глубокую связь с природой. Когда ты разглядываешь бескрайние просторы океана, ты невольно понимаешь, насколько малы мы во вселенной. Море – это могущественная сила, способная сменить направление твоей жизни в мгновение ока. Вся история человечества пронизана подвигами, сделанными на его волнах. Подняв паруса и отпустив швартовые веревки, путешествуешь из порта в порт, погружаясь в морские приключения. Каждая поездка на судне – это искусство покорения ветра и волн, единение с элементами. Море – поистине учитель жизни, обучающий тебя не только справляться с непредсказуемыми силами природы, но и находить гармонию с самим собой. Веками люди исследовали неизведанные уголки нашей планеты, отправляясь в далекие плавания, в поисках новых земель, приключений, богатств и познания. Основатели новых цивилизаций были искателями приключений, смельчаками, способными покорить море, даже если это означало преодоление своих собственных страхов. Однако путешествия по морю – это не только оставить привычную сушу позади. Это и возможность заглянуть в глубины своей души, преодолевать внутренние барьеры, о которых ты раньше и не задумывался. Вдали от суеты и шума городской жизни, ты можешь переосмыслить свои приоритеты, обрести внутренний покой и вдохновение. Находясь на судне, пребываешь в постоянной гармонии с водными просторами. Они подчиняются своим законам, и лишь благодаря доверию к морю и своим навыкам, можно извлечь из этой силы наилучшую выгоду. Путешествуя по морю, познаешь и обретаешь себя, расширяешь свои границы и погружаешься в бесконечный поток морской энергии.', author: 'А. С. Пушкин', image: 'static/img/post-1.jpg', date: '08.10.2023'},
@@ -38,15 +43,37 @@ function postout (i) {
   document.getElementById('simage').src = records[i].image
 }
 
-// Кнопка "Изменить"
-let singleButton = document.createElement('button')
-singleButton.className = 'blogcard-button'
-singleButton.innerHTML = 'Изменить'
-document.getElementById('root').appendChild(singleButton)
+// // Кнопка "Изменить"
+// let singleButton = document.createElement('button')
+// singleButton.className = 'blogcard-button'
+// singleButton.innerHTML = 'Изменить'
+// document.getElementById('root').appendChild(singleButton)
 
-singleButton.addEventListener('click', () => {
+// singleButton.addEventListener('click', () => {
+//   let a
+//   // a = 'config.html'
+//   // document.location.assign(a)
+//   a = document.getElementById('gh').value
+//   localStorage.setItem('mas', a)
+//   let sin = localStorage.getItem('mas')
+//   console.log(sin)
+// })
+
+// document.getElementById('glebtn').addEventListener('click', () => {
+//   let a
+//   a = 'prim.html'
+//   document.location.assign(a)
+// })
+
+  // Кнопка "Читать полностью"
+let sButton = document.createElement('button')
+sButton.className = 'blogcard-button'
+sButton.innerHTML = 'Редактировать'
+document.getElementById('sbtn').appendChild(sButton)
+
+sButton.addEventListener('click', () => {
   let a
-  a = 'config.html'
+  a = 'edit.html'
   document.location.assign(a)
 })
 
