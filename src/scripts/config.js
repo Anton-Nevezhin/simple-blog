59005ed39cf3e31d4604ef903eb9fbaa7f2f5362
anton-nevezhin <anton-nevezhin@yandex.ru>
2023-11-11 23:32:58 +0300

    Add edit

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,8 +1,26 @@
 // Должно вывести этот текст
 console.log('Должно быть в консоли config')
 
-import { recSingle } from '../scripts/single.js'
-// Должно вывести 'Из single'
-console.log('Из single. Должно быть в консоли config: ', recSingle)
+// import { recSingle } from '../scripts/single.js'
+// // Должно вывести 'Из single'
+// console.log('Из single. Должно быть в консоли config: ', recSingle)
 
-export let rec = 'Пример текста'
+// export let rec = 'Пример текста'
+
+let editmas = localStorage.getItem('mas')
+console.log('mas in config: ', editmas)
+console.log('Simple text in config')
+
+// Кнопка "Изменить"
+let singleButton = document.createElement('button')
+// singleButton.className = 'blogcard-button'
+singleButton.innerHTML = 'Изменить'
+document.getElementById('recconfig').appendChild(singleButton)
+
+singleButton.addEventListener('click', () => {
+  let a
+  // a = 'config.html'
+  // document.location.assign(a)
+  a = document.getElementById('ed').value
+  localStorage.setItem('mas', a)
+})
